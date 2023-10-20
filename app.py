7c3473afec28053f65500162b8681ed71edf47ae
ibnAlQyyim <141359042+ibnAlQyyim@users.noreply.github.com>
2023-10-20 22:51:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
+import subprocess
+import os
+os.environ["SYSTEM"] = ""
+os.environ["SPACE_ID"] = ""
+
+#os.system("apt install -y sudo wget curl git xfce4")
+#os.system("echo -e '1234\n1234'| passwd")
+#os.system("pip install webssh")
+#os.system("wssh --port=7860")
+
 import gradio as gr
-def greet(name):
-    return "Hello" + name + "!!"
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+
+def execute_terminal_command(command):
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        
+        output = result.stdout.strip() if result.stdout else result.stderr.strip()
+        return output
+    except Exception as e:
+        return str(e)
+
+def terminal_interface(command):
+    output = execute_terminal_command(command)
+    return output
+
+iface = gr.Interface(fn=terminal_interface, inputs="text", outputs="text", title="Terminal Interface")
+iface.launch(share=True)
