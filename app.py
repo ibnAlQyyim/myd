77052b5cd09a5f4dbcfa60c6100b0f93c83ee7a1
ibnAlQyyim <141359042+ibnAlQyyim@users.noreply.github.com>
2024-08-13 19:50:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import subprocess
-import os
-os.environ["SYSTEM"] = ""
-os.environ["SPACE_ID"] = ""
 
-#os.system("apt install -y sudo wget curl git xfce4")
-#os.system("echo -e '1234\n1234'| passwd")
-#os.system("pip install webssh")
-#os.system("wssh --port=7860")
-
+from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
+from pyrogram import Client, filters
+import os, time, yt_dlp, wget, subprocess
+from datetime import timedelta
 import gradio as gr
+from multiprocessing import Process
 
 def execute_terminal_command(command):
     try:
@@ -22,6 +18,113 @@ def execute_terminal_command(command):
 def terminal_interface(command):
     output = execute_terminal_command(command)
     return output
-
+  
 iface = gr.Interface(fn=terminal_interface, inputs="text", outputs="text", title="Terminal Interface")
-iface.launch(share=True)
+
+app = Client(
+    "yt_bot",
+    api_id=22174278,
+    api_hash="963743dc84addd651cd295e523856ece",
+    bot_token="6230806083:AAGDAG762pEutljbwYES1Vf0MFr-uRQTP2Y"
+)
+
+
+@app.on_message(filters.command('start'))
+def start(client, message):
+    client.send_message(message.chat.id, "Hi I'am ytdlp", disable_web_page_preview=True)
+
+@app.on_message(filters.regex(r'^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?$'))
+def url(client, message):
+    buttons = [
+            [InlineKeyboardButton("مقطع", callback_data="format_video")],
+            [InlineKeyboardButton("صوت", callback_data="format_audio")],
+            [InlineKeyboardButton("إلغاء", callback_data="cancel")]
+        ]
+
+    reply_markup = InlineKeyboardMarkup(buttons)
+    print(message.id)
+    message.reply("اختر:", reply_markup=reply_markup, reply_to_message_id=message.id)
+
+@app.on_callback_query(filters.regex(r'format_'))
+def quality(client, callback_query: CallbackQuery):
+    typ = callback_query.data.split("_")[1]
+    buttons = [
+            [InlineKeyboardButton("low", callback_data=f"quality_{typ}_low")],
+            [InlineKeyboardButton("medium", callback_data=f"quality_{typ}_medium")],
+            [InlineKeyboardButton("best", callback_data=f"quality_{typ}_best")],
+            [InlineKeyboardButton("إلغاء", callback_data="cancel")]
+        ]
+
+    reply_markup = InlineKeyboardMarkup(buttons)
+    client.edit_message_text(callback_query.from_user.id, message_id=callback_query.message.id, text="quality", reply_markup=reply_markup)
+
+@app.on_callback_query(filters.regex(r'quality_'))
+def download(client, callback_query: CallbackQuery):
+    print("download")
+    message = client.get_messages(callback_query.from_user.id, message_ids=callback_query.message.reply_to_message.id)
+#    progress_message = client.send_message(message.chat.id, '\U0001F4E5**Downloading ...**', reply_to_message_id=message.id)
+    client.edit_message_text(callback_query.from_user.id, message_id=callback_query.message.id, text="downloading....")
+    typ = callback_query.data.split("_")[1]
+    qty = callback_query.data.split("_")[2]
+
+    url = message.text
+    if typ == "video":
+        print("video")
+
+        if qty == "low":
+            qtyfmt = "worst"
+
+        if qty == "medium":
+            qtyfmt = "18"
+
+        if qty == "best":
+            qtyfmt = "best[ext=mp4]+bestaudio"
+
+        ydl_opts = {"format": qtyfmt}
+        path = ""
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            path = ydl.prepare_filename(info)
+
+        title = info['title']
+        dur =  info['duration']
+        perf = info['channel']
+        thumbnail = wget.download(info['thumbnail'])
+        width = info['width']
+        height = info['height']
+        app.send_video(message.chat.id, path, caption=f"**{title}***", duration=dur, width=width, height=height, thumb=thumbnail, reply_to_message_id=message.id)
+
+    if typ == "audio":
+        print("audio")
+
+        if qty == "low":
+            qtyfmt = "139"
+
+        if qty == "medium":
+            qtyfmt = "139-drc"
+
+        if qty == "best":
+            qtyfmt = "bestaudio[ext=m4a]"
+
+        ydl_opts = {"format": qtyfmt}
+        path = ""
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            path = ydl.prepare_filename(info)
+
+        title = info['title']
+        dur =  info['duration']
+        perf = info['channel']
+        thumbnail = wget.download(info['thumbnail'])
+        channel_name = info['channel']
+        client.send_audio(message.chat.id, audio=path, duration=dur, performer=perf, title=title, caption=f"**{title}**", reply_to_message_id=message.id, thumb=thumbnail)
+
+    os.system(f"rm -rf '{thumbnail}' '{path}'")
+
+
+if __name__ == '__main__':
+    Process(target=iface.launch).start()   
+try:
+    app.run()
+except Exception as e:
+    print("Error:",e)
